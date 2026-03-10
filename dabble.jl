8b8bf2076c37c72744d82e72c2d3ac828b027529
Christoph Zetek <christoph.zetek@tuhh.de>
2026-03-10 21:13:01 +0100

    updated w/ week4 res & week5 matches

--- dabble.jl
+++ dabble.jl
@@ -3,18 +3,21 @@ using BalatrELO
 
 leagueDataFN = "src\\leagueData\\"
 
-playerFilename = leagueDataFN*"players_k20_w3.csv"
-outputPlayerFileName = leagueDataFN*"players_k20_w4.csv"
-matchFilename = leagueDataFN*"matches_w4.csv"
-#resultFilename = leagueDataFN*"results_w3.csv"
+playerFilename = leagueDataFN*"players_k20_w4.csv"
+outputPlayerFileName = leagueDataFN*"players_k20_w5.csv"
+matchFilename = leagueDataFN*"matches_w5.csv"
+resultFilename = leagueDataFN*"results_w5.csv"
 allResultsFilename = leagueDataFN*"allResults_k20.csv"
 
 players = readPlayers(filename = playerFilename)
 matches = readNewMatches(players,filename=matchFilename)
-#results = readNewResults(matches,filename=resultFilename)
-#updateAllResults!(results,filename=allResultsFilename)
-#updateAllRatings!(results)
-#writePlayers!(players,filename=outputPlayerFileName)
-#stepThroughAllWeeks(leagueDataFN*"players_k20_w",leagueDataFN*"matches_w",leagueDataFN*"results_w",leagueDataFN*"allResults_k20.csv",2)
 giveExpectedScores(matches)
 giveMatchdayExpectedValue(matches)
+
+#results = readNewResults(matches,filename=resultFilename)
+#updateAllRatings!(results)
+#writePlayers!(players,filename=outputPlayerFileName)
+#updateAllResults!(results,filename=allResultsFilename)
+
+
+#stepThroughAllWeeks(leagueDataFN*"players_k20_w",leagueDataFN*"matches_w",leagueDataFN*"results_w",leagueDataFN*"allResults_k20.csv",2)
